d1b6730d5304142b6222683e2da7d01ec642ef9a
Simondin <smondima89@gmail.com>
2014-09-25 16:36:45 +0200

    First commit
    Inizio applicazione per il diario diabetologico

---
+++ app/src/main/java/simone/diario/Fragment/MeasureFragment.java
@@ -0,0 +1,32 @@
+package simone.diario.Fragment;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import simone.diario.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class MeasureFragment extends Fragment {
+
+
+    public MeasureFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_measure, container, false);
+    }
+
+
+}
